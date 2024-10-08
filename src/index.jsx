7a129d00211b139d85daace52f349db8cdf17981
Bhuvanesh Duvvuri <duvvuribhuvanesh@zohomail.in>
2024-10-09 05:17:03 +0530

    Done

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import "vedhya/css/vedhya.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const container = document.querySelector(".root")!;
+const container = document.querySelector(".root");
 const root = createRoot(container);
 root.render(
   <Router>
