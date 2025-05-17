1a7bba7415f3a48aff519e9ef1eeb72df585061a
Bhuvanesh Duvvuri <duvvuribhuvanesh@zohomail.in>
2025-05-17 06:19:22 +0530

    feat: add generator functionality for creating React projects

---
+++ dist/generator.d.ts
@@ -0,0 +1,2 @@
+import { GeneratorOptions, GeneratorResult } from "./types.js";
+export declare const generateReactProject: ({ projectName, authorName, targetDirectory }: GeneratorOptions) => Promise<GeneratorResult>;
