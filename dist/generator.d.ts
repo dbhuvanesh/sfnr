27fecae7de83fc0d57d2bd124f73d6107f407c34
Bhuvanesh Duvvuri <duvvuribhuvanesh@zohomail.in>
2025-05-17 05:35:11 +0530

    refactor: remove generator files and types for project structure cleanup

--- dist/generator.d.ts
+++
@@ -1,2 +0,0 @@
-import { GeneratorOptions, GeneratorResult } from "./types.js";
-export declare const generateReactProject: ({ projectName, authorName, targetDirectory }: GeneratorOptions) => Promise<GeneratorResult>;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import { generateReactProject } from './generator.js';
-export { generateReactProject };
